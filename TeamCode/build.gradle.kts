732453e9ccd4d3946adf6199882d26e1ee5a1f0e
Oscar Chevalier <oscar.chevalier@gmail.com>
2025-08-18 17:45:27 +1000

    chore: updated test module plugins

--- TeamCode/build.gradle.kts
+++ TeamCode/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.2.0-0.1.3"
+	id("dev.frozenmilk.teamcode") version "10.3.0-0.1.4"
 	id("dev.frozenmilk.sinister.sloth.load") version "0.2.2"
 }
 
